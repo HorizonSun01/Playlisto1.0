308ad2e539586e59996f66fc905a1381d69f801b
HorizonSun01 <ofekshemesh@gmail.com>
2024-12-08 14:01:51 +0200

    starting

---
+++ playlisto/src/App.jsx
@@ -0,0 +1,21 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  return (
+    <div className="app">
+      <header className="app-header">
+        <h1>Playlisto</h1>
+      </header>
+      <main className="app-main">
+        <div className="button-container">
+          <button className="primary-button">Start Game</button>
+          <button className="primary-button">Join Room</button>
+          <button className="primary-button">Daily Challenge</button>
+        </div>
+      </main>
+    </div>
+  )
+}
+
+export default App 
